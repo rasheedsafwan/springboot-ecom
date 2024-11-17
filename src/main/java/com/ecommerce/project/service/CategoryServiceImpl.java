f9d86176777e3b2d05af30f6c2c65c4d0ec82198
rasheedsafwan <rasheedsafwan23@gmail.com>
2024-11-17 18:05:16 +0530

    added PutMapping

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -7,11 +7,13 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories = new ArrayList<>();
     private Long nextId = 1L;
+
     @Override
     public List<Category> getAllCategories() {
         return categories;
@@ -34,5 +36,19 @@ public class CategoryServiceImpl implements CategoryService{
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " deleted successfully";
 
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory = categories.stream()
+                .filter(c -> c.getCategoryId()==categoryId)
+                .findFirst();
+        if (optionalCategory.isPresent()){
+            Category existingCategory= optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "category not found");
+        }
     }
 }
