519ae55296ec7c0355f384379e37cc952bdc36da
rasheedsafwan <rasheedsafwan23@gmail.com>
2024-11-25 15:16:31 +0530

    validations & code cleanup

---
+++ src/main/java/com/ecommerce/project/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,24 @@
+package com.ecommerce.project.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    String resourceName;
+    String field;
+    String fieldName;
+    long fieldId;
+
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String resourceName, String field, String fieldName){
+        super(String.format("%s not found with %s: %s", resourceName, field, fieldName ));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldName = fieldName;
+    }
+    public ResourceNotFoundException(String resourceName, String field, long fieldId){
+        super(String.format("%s not found with %s: %d", resourceName, field, fieldId ));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldId = fieldId;
+    }
+}
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,51 +1,50 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
-    private List<Category> categories = new ArrayList<>();
-    private Long nextId = 1L;
 
     @Autowired
     private CategoryRepository categoryRepository;
 
-
     @Override
     public List<Category> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty())
+            throw new APIException("No categories found");
         return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
+        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+        if (savedCategory != null)
+            throw new APIException("Category with the name " + category.getCategoryName() +" already exists");
         categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
         Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "resource not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("category", "categoryId", categoryId));
 
         categoryRepository.delete(category);
         return "Category with categoryId: " + categoryId + " deleted successfully";
-
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
 
         Category savedCategory = categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "category not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("category", "categoryId", categoryId));
 
         category.setCategoryId(categoryId);
         savedCategory = categoryRepository.save(category);
